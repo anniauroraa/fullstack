fe8be46e96e8d1903dec11ee08be761b0f65ddaf
Anni <anni.a.virtanen@tuni.fi>
2024-01-18 18:11:02 +0200

    refactored the file order in the repository

---
+++ osa1/testit/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+const Display = ({ counter }) => {
+    return (
+        <div>{counter}</div>
+    )
+}
+
+const Button = (props) => {
+    return (
+      <button onClick={props.handleClick}>
+        {props.text}
+      </button>
+    )
+}
+
+const App = () => {
+
+  const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
+  return (
+    <div>
+      <Display counter={counter}/>
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />
+    </div>
+  )
+}
+
+export default App
